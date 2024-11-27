6452c133f8f8b318c2cf6804d96878e338fc29f9
Wall_Liu <tuongthanhien@gmail.com>
2024-11-27 09:19:21 +0700

    init project

---
+++ src/main/java/org/example/doancuoiky/DoAnCuoiKyApplication.java
@@ -0,0 +1,13 @@
+package org.example.doancuoiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnCuoiKyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoAnCuoiKyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/doancuoiky/DoAnCuoiKyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doancuoiky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnCuoiKyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
